2867f06275a892e6b54c25eb40e3ba63e5b89504
Leonardo Matsubara <leo.mikkienzo@gmail.com>
2025-10-27 17:06:26 -0300

    refine

--- src/extract_canvas.py
+++ src/extract_canvas.py
@@ -10,9 +10,9 @@ token = os.getenv('canvas_token')
 headers = {'Authorization': f'Bearer {token}'}
 cert = certifi.where()
 course_id = 15812
+quiz_id = None
 api_assignment = f'/api/v1/courses/{course_id}/assignments'
-#api_report = f'/api/v1/courses/{course_id}/quizzes/{quizzes}/reports'
-
+api_report = f'/api/v1/courses/{course_id}/quizzes/{quiz_id}/reports'
 
 # Extrai quiz_id com base no período buscado, armazena input para parâmetro
 ano = input('Qual o ano do report? ').strip()
@@ -20,8 +20,8 @@ mes = input('Qual o mês do report? ').strip().capitalize()
 search_conc = f'{mes} {ano}'
 params = {'search_term': search_conc}
 
-def captura_assignment():
-    quiz_id = None
+# utilizar API para descobrir o quizzes correto com base no nome do assignment
+def captura_assignment():   
     try:
         url_s1 = f'{CANVAS_API_URL}{api_assignment}'
         response = requests.get(headers=headers,
@@ -36,10 +36,9 @@ def captura_assignment():
             print(f'{response.status_code}: {response.text}')
 
     except Exception as e:
-        print(f'Ocorreu um erro: {e}')
+        print(f'Ocorreu um erro ao capturar assignment: {e}')
     return quiz_id
 
-captura_assignment()
+#def dwl_teste():
 
-# utilizar API para descobrir o quizzes correto com base no nome do assignment
 #"report_type": "student_analysis",55049
