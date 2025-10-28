5b46d195b3a61e2b3077b31b2eb60b1eddc1ca34
Leonardo Matsubara <leo.mikkienzo@gmail.com>
2025-10-28 16:22:20 -0300

    Finish One Step - Ref

--- src/extract_canvas.py
+++ src/extract_canvas.py
@@ -2,43 +2,78 @@ import requests
 import certifi
 from dotenv import load_dotenv, find_dotenv
 import os
+import re
+from pathlib import Path
 
 # Config Globais
+CURRENT_DIR = Path(__file__).resolve().parent
 CANVAS_API_URL = 'https://famonline.instructure.com'
 load_dotenv(find_dotenv(filename='canvas_tkn.env'))
 token = os.getenv('canvas_token')
 headers = {'Authorization': f'Bearer {token}'}
 cert = certifi.where()
 course_id = 15812
-quiz_id = None
-api_assignment = f'/api/v1/courses/{course_id}/assignments'
-api_report = f'/api/v1/courses/{course_id}/quizzes/{quiz_id}/reports'
+path_download = rf'{CURRENT_DIR.parent}/data/raw'
 
 # Extrai quiz_id com base no período buscado, armazena input para parâmetro
 ano = input('Qual o ano do report? ').strip()
 mes = input('Qual o mês do report? ').strip().capitalize()
-search_conc = f'{mes} {ano}'
-params = {'search_term': search_conc}
 
-# utilizar API para descobrir o quizzes correto com base no nome do assignment
-def captura_assignment():   
+# utilizar API para descobrir o quizzes correto com base no nome da assignment
+def catch_assignment():
+    search_conc = f'{mes} {ano}'
+    params = {'search_term': search_conc} 
     try:
-        url_s1 = f'{CANVAS_API_URL}{api_assignment}'
+        url = f'{CANVAS_API_URL}/api/v1/courses/{course_id}/assignments'
         response = requests.get(headers=headers,
-                                url=url_s1,
+                                url=url,
                                 verify=cert,
                                 params=params)
         if response.status_code == 200:
             result_assignment = response.json()
             quiz_id = result_assignment[0].get('quiz_id')
-            print(quiz_id)
         else:
             print(f'{response.status_code}: {response.text}')
-
     except Exception as e:
         print(f'Ocorreu um erro ao capturar assignment: {e}')
     return quiz_id
 
-#def dwl_teste():
+def catch_link_report():
+    quiz_id = catch_assignment()
+    params = {'quiz_report[report_type]':'student_analysis','include':'file'}
+    print('\nRelatório em processamento...\n')
+    try:
+        url= f'{CANVAS_API_URL}/api/v1/courses/{course_id}/quizzes/{quiz_id}/reports'
+        response = requests.post(headers=headers,
+                                url=url,
+                                verify=cert,
+                                params=params)
+        if response.status_code == 200:
+            result = response.json()
+            report_name = result['file']['display_name']
+            report_link = result['file']['url']
+            print(f'Relatório Gerado: {report_name}')
+        else:
+            print(f'{response.status_code}: {response.text}')
+            return None, None
+    except Exception as e:
+        print(f'Ocorreu um erro ao realizar o download: {e}')
+    return report_name, report_link
 
-#"report_type": "student_analysis",55049
+def download_save(report_name, report_link):
+    safe_name = re.sub(r'[<>:"/\\|?*]', '-', report_name)
+    response = requests.get(report_link,
+                            headers=headers,
+                            verify=cert)
+    if response.status_code == 200:
+        file_path = f'{path_download}/{safe_name}'
+        with open(file_path, "wb") as file:    
+            file.write(response.content)
+        print(f'Arquivo salvo em: {file_path}')
+    else:
+            print(f'{response.status_code}: {response.text}')
+
+#Teste
+
+report_name, report_link = catch_link_report()
+download_save(report_name, report_link)
